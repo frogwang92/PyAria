a4c808b48f4f7f2f70cae53e9c257fe500feb246
frogwang92 <frogwang92@gmail.com>
2016-11-22 22:06:07 +0800

    broker

--- broker/backtestbroker.py
+++ broker/backtestbroker.py
@@ -5,11 +5,19 @@ class BackTestBroker(object):
     def buy(self, position, percentage):
         pass
 
-    def sell(self):
+    def sell(self, percentage):
         pass
 
 
 class Portfolio(object):
-    def __init__(self, money, position):
-        self.__money = money
-        self.__position = position
+    """support one hold only"""
+    def __init__(self, money = 1000000, holdshare = 0, position = 0):
+        """position = 0 means there is no holdshare in the portfolio"""
+        self.money = money
+        self.position = position
+        self.holdshare = holdshare
+
+    def portfolio_change(self, money, holdshare, position):
+        self.money += money
+        self.holdshare += holdshare
+        self.position = position
